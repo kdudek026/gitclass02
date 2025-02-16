564dd2a71f5ce86163d3f1e36b03eea6ca02e97c
kdudek026 <kdudek026@gmail.com>
2025-02-16 15:17:28 -0600

    first commit

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world 2");
+
+    }
+}
